923a657ab5e7db940b18ad6f8bc152c7a65fce52
Vu Tran <vu@vu-tran.com>
2016-11-01 22:34:25 -0700

    mock recursive-readdir

---
+++ __mocks__/recursive-readdir.js
@@ -0,0 +1,34 @@
+let mockFiles = [];
+let mockError = false;
+
+/**
+ * Reads the directory and apply the callback
+ * with the mocked values
+ *
+ * @param {String} directory
+ * @param {Function} callback
+ */
+const recursiveReaddir = (directory, callback) => {
+  callback.call(null, mockError, mockFiles);
+};
+
+/**
+ * Sets the mock return value for recursive-readdir
+ *
+ * @param {String[]} files - An array of files
+ */
+ // eslint-disable-next-line no-underscore-dangle
+recursiveReaddir.__setFiles = (files) => {
+  mockFiles = files;
+};
+
+/**
+ * Set a flag to return an error or not
+ *
+ * @param {Boolean} flag - Set to true to return an err
+ */
+recursiveReaddir.__setReturnError = (flag) => {
+  mockError = flag;
+};
+
+module.exports = recursiveReaddir;
--- __tests__/index.js
+++ __tests__/index.js
@@ -1,13 +1,13 @@
 import m from '../';
 
-jest.mock('fs');
+jest.mock('recursive-readdir');
 
 describe('os-fonts', () => {
-  const fs = require('fs');
+  const recursive = require('recursive-readdir');
 
   it('should retrieve some fonts', async () => {
     // set up mock data
-    fs.__setFiles([
+    recursive.__setFiles([
       'foo.ttf',
       'bar.ttf',
     ]);
@@ -16,21 +16,21 @@ describe('os-fonts', () => {
   });
 
   it('should retrieve no fonts', async () => {
-    fs.__setFiles([]);
+    recursive.__setFiles([]);
     const fonts = await m.getAll();
     expect(fonts.length).toBe(0);
   });
 
   it('should retrieve user fonts', async () => {
     // set up mock data
-    fs.__setFiles([]);
+    recursive.__setFiles([]);
     const fonts = await m.getAll('user');
     expect(fonts.length).toBe(0);
   });
 
   it('should get all network fonts', async () => {
     // set up mock data
-    fs.__setFiles([
+    recursive.__setFiles([
       'foo.ttf',
       'bar.ttf',
       'baz.ttf',
@@ -41,7 +41,7 @@ describe('os-fonts', () => {
   });
 
   it('should return nothing for readdir errors', async () => {
-    fs.__setReturnError(true);
+    recursive.__setReturnError(true);
     const fonts = await m.getAll('network');
     expect(fonts.length).toBe(0);
   });
