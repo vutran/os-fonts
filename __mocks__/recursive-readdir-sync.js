60f6f175399cc1c2c8c437b4a41299a22eeaaa01
Vu Tran <vutran@dropbox.com>
2017-10-08 19:28:48 -0700

    Add missing tests for sync

---
+++ __mocks__/recursive-readdir-sync.js
@@ -0,0 +1,35 @@
+let mockFiles = [];
+let mockError = false;
+
+/**
+ * Reads the directory and apply the callback
+ * with the mocked values
+ *
+ * @param {String} directory
+ */
+const recursiveReaddirSync = (directory) => {
+    if (mockError) {
+        throw new Error(true);
+    }
+    return mockFiles;
+};
+
+/**
+ * Sets the mock return value for recursive-readdir
+ *
+ * @param {String[]} files - An array of files
+ */
+recursiveReaddirSync.__setFiles = files => {
+    mockFiles = files;
+};
+
+/**
+ * Set a flag to return an error or not
+ *
+ * @param {Boolean} flag - Set to true to return an err
+ */
+recursiveReaddirSync.__setReturnError = flag => {
+    mockError = flag;
+};
+
+module.exports = recursiveReaddirSync;
--- __tests__/index.js
+++ __tests__/index.js
@@ -1,40 +1,79 @@
 import m from '../';
 
 jest.mock('recursive-readdir');
+jest.mock('recursive-readdir-sync');
 
 describe('os-fonts', () => {
-    const recursive = require('recursive-readdir');
+    describe('async', () => {
+        const recursive = require('recursive-readdir');
+        it('should retrieve some fonts', async () => {
+            // set up mock data
+            recursive.__setFiles(['foo.ttf', 'bar.ttf']);
+            const fonts = await m.getAll();
+            expect(fonts.length).toBe(2);
+        });
 
-    it('should retrieve some fonts', async () => {
-        // set up mock data
-        recursive.__setFiles(['foo.ttf', 'bar.ttf']);
-        const fonts = await m.getAll();
-        expect(fonts.length).toBe(2);
+        it('should retrieve no fonts', async () => {
+            recursive.__setFiles([]);
+            const fonts = await m.getAll();
+            expect(fonts.length).toBe(0);
+        });
+
+        it('should retrieve user fonts', async () => {
+            // set up mock data
+            recursive.__setFiles([]);
+            const fonts = await m.getAll('user');
+            expect(fonts.length).toBe(0);
+        });
+
+        it('should get all network fonts', async () => {
+            // set up mock data
+            recursive.__setFiles(['foo.ttf', 'bar.ttf', 'baz.ttf', 'qux.ttf']);
+            const fonts = await m.getAll('network');
+            expect(fonts.length).toBe(4);
+        });
+
+        it('should return nothing for readdir errors', async () => {
+            recursive.__setReturnError(true);
+            const fonts = await m.getAll('network');
+            expect(fonts.length).toBe(0);
+        });
     });
 
-    it('should retrieve no fonts', async () => {
-        recursive.__setFiles([]);
-        const fonts = await m.getAll();
-        expect(fonts.length).toBe(0);
+    describe('sync', () => {
+        const recursiveSync = require('recursive-readdir-sync');
+        it('should retrieve some fonts', async () => {
+            // set up mock data
+            recursiveSync.__setFiles(['foo.ttf', 'bar.ttf']);
+            const fonts = await m.getAllSync();
+            expect(fonts.length).toBe(2);
+        });
+
+        it('should retrieve no fonts', async () => {
+            recursiveSync.__setFiles([]);
+            const fonts = await m.getAllSync();
+            expect(fonts.length).toBe(0);
+        });
+
+        it('should retrieve user fonts', async () => {
+            // set up mock data
+            recursiveSync.__setFiles([]);
+            const fonts = await m.getAllSync('user');
+            expect(fonts.length).toBe(0);
+        });
+
+        it('should get all network fonts', async () => {
+            // set up mock data
+            recursiveSync.__setFiles(['foo.ttf', 'bar.ttf', 'baz.ttf', 'qux.ttf']);
+            const fonts = await m.getAllSync('network');
+            expect(fonts.length).toBe(4);
+        });
+
+        it('should return nothing for readdir errors', async () => {
+            recursiveSync.__setReturnError(true);
+            const fonts = await m.getAllSync('network');
+            expect(fonts.length).toBe(0);
+        });
     });
 
-    it('should retrieve user fonts', async () => {
-        // set up mock data
-        recursive.__setFiles([]);
-        const fonts = await m.getAll('user');
-        expect(fonts.length).toBe(0);
-    });
-
-    it('should get all network fonts', async () => {
-        // set up mock data
-        recursive.__setFiles(['foo.ttf', 'bar.ttf', 'baz.ttf', 'qux.ttf']);
-        const fonts = await m.getAll('network');
-        expect(fonts.length).toBe(4);
-    });
-
-    it('should return nothing for readdir errors', async () => {
-        recursive.__setReturnError(true);
-        const fonts = await m.getAll('network');
-        expect(fonts.length).toBe(0);
-    });
 });
