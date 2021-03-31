1e91e8b261c26637143635a433ee71c0396ad0d5
Junyoung Park <jypark@siliconbridge.co.kr>
2021-03-31 14:11:40 +0900

    Fix indent

--- index.d.ts
+++ index.d.ts
@@ -1,39 +1,39 @@
 type UseType = 'user' | 'local' | 'network' | 'system';
 
 declare module 'os-fonts' {
-const osFonts: {
-/**
- * Retrieve all font files in the given directory.
- *
- * @param dir The directory to read
- * @return Array of fonts
- */
-getFontsInDirectory: (dir: string) => Promise<string[]>;
+    const osFonts: {
+        /**
+         * Retrieve all font files in the given directory.
+         *
+         * @param dir The directory to read
+         * @return Array of fonts
+         */
+        getFontsInDirectory: (dir: string) => Promise<string[]>;
 
-/**
- * Retrieve all font files in the given directory (synchronous).
- *
- * @param dir The directory to read
- * @return Array of fonts
- */
-getFontsInDirectorySync: (dir: string) => string[];
+        /**
+         * Retrieve all font files in the given directory (synchronous).
+         *
+         * @param dir The directory to read
+         * @return Array of fonts
+         */
+        getFontsInDirectorySync: (dir: string) => string[];
 
-/**
- * Get all fonts.
- *
- * @param useType The use type. Default is 'system'.
- * @return An array of fonts available
- */
-getAll: (useType?: UseType) => Promise<string[]>;
+        /**
+         * Get all fonts.
+         *
+         * @param useType The use type. Default is 'system'.
+         * @return An array of fonts available
+         */
+        getAll: (useType?: UseType) => Promise<string[]>;
 
-/**
- * Get all fonts (synchronous).
- *
- * @param useType The use type. Default is 'system'.
- * @return An array of fonts available
- */
-getAllSync: (useType?: UseType) => string[];
+        /**
+         * Get all fonts (synchronous).
+         *
+         * @param useType The use type. Default is 'system'.
+         * @return An array of fonts available
+         */
+        getAllSync: (useType?: UseType) => string[];
 
-};
-export = osFonts;
+    };
+    export = osFonts;
 }
